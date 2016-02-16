da866af4bba8a7b5bd9e12c05471989bf965a1de
rpalamut <rahulpalamut@gmail.com>
2016-02-15 21:58:33 -0800

    have points plotted

--- states.py
+++ states.py
@@ -18,7 +18,8 @@ state_ys = [states[code]["lats"] for code in states]
 state_xs = state_xs[0:5] + state_xs[6:16] + state_xs[17:18] + state_xs[19:31] + state_xs[32:46]
 state_ys = state_ys[0:5] + state_ys[6:16] + state_ys[17:18] + state_ys[19:31] + state_ys[32:46]
 
-
+x = [1,2,3]
+y = [4,5,6]
 source = ColumnDataSource(data=dict(x=state_xs, y=state_ys))
 source2 = ColumnDataSource(data=dict(x=x, y=y))
 
